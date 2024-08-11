4c780511a9a07004e076efa6e46ce5ee27f02666
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2024-08-11 19:39:13 +0530

    Time: 66 ms (29.81%) | Memory: 51.1 MB (92.75%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    let count = 0
+    let maxi = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            count += 1
+            maxi = Math.max(maxi, count)
+        } else {
+            count = 0
+        }
+    }
+    return maxi
+};
