82c585a7683e1ceb07d478bfe739c70927a1ab70
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2024-11-27 12:30:15 +0530

    Time: 4 ms (32.56%) | Memory: 58.7 MB (84.38%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -2,19 +2,23 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
-    */
-var rotate = function(nums, k) {
-    let n = nums.length
+ */
+var rotate = function (nums, k) {
+    let n = nums.length;
     k = k % n
-    let temp = []
+    let temp = [];
 
-    for(let i = n-k;i<=n;i++ ){
+    for (let i = n - k; i < n; i++) {
         temp.push(nums[i])
     }
-    for(let i = n-1; i>=k; i-- ){
-        nums[i] = nums[i-k]
+
+    for (let i = n - 1; i >= k; i--) {
+        nums[i] = nums[i - k]
     }
-    for(let i = 0;i<k ;i++ ){
+
+    for (let i = 0; i < k; i++) {
         nums[i] = temp[i]
     }
+
+    return nums
 };
