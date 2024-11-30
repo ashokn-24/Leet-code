7381337f38571913c7104c6ffd36d05bb856302f
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2024-11-30 13:12:34 +0530

    Time: 5 ms (54.36%) | Memory: 50.9 MB (46.46%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,13 +3,20 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum = function (nums, k) {
+    let mp = new Map();
+    let ans = []
 
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [j, i]
-            }
+        let complement = k - nums[i] 
+
+        if (mp.has(complement)) {
+            ans = [mp.get(complement),i]
+        }
+
+        if(!mp.has(nums[i])){
+            mp.set(nums[i],i)
         }
     }
+    return ans;
 };
