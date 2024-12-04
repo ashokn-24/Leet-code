4abf85f2ca069ece36f50818bdbd2bb7b25fb160
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2024-12-04 23:53:09 +0530

    Time: 28 ms (47.18%) | Memory: 91.1 MB (67.56%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -3,19 +3,22 @@
  * @return {number[]}
  */
 var rearrangeArray = function (arr) {
-    let positive = []
-    let negative = []
-    let nums = []
-    for (let i = 0; i < arr.length; i++) {
+    let pos = []
+    let neg = []
+    let n = arr.length
+
+    for (let i = 0; i < n; i++) {
         if (arr[i] < 0) {
-            negative.push([arr[i]])
+            neg.push(arr[i])
         } else {
-            positive.push([arr[i]])
+            pos.push(arr[i])
         }
     }
-    for (let i = 0; i < arr.length / 2; i++) {
-        arr[2 * i] = positive[i]
-        arr[2 * i + 1] = negative[i]
+
+    for (let i = 0; i < n / 2; i++) {
+        arr[2 * i] = pos[i];
+        arr[2 * i + 1] = neg[i]
     }
-    return arr
+
+    return arr;
 };
