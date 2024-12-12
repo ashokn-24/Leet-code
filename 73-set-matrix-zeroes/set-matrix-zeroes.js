7a4e18db278236aff5c62cd53592f32b7088c0bf
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2024-12-12 13:13:01 +0530

    Time: 1 ms (94.53%) | Memory: 53.6 MB (56.61%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -5,39 +5,27 @@
 var setZeroes = function (matrix) {
     let n = matrix.length;
     let m = matrix[0].length;
+    let row = new Array(n).fill(0);
+    let col = new Array(m).fill(0);
 
-    const markRow = (matrix, m, n, i) => {
+    for (let i = 0; i < n; i++) {
         for (let j = 0; j < m; j++) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = -10;
-            }
-        }
-    }
-
-    const markCol = (matrix, m, n, j) => {
-        for (let i = 0; i < n; i++) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = -10
+            if (matrix[i][j] === 0) {
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
 
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                markRow(matrix, m, n, i)
-                markCol(matrix, m, n, j)
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0
             }
         }
     }
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (matrix[i][j] == -10) {
-                matrix[i][j] = 0;
-            }
-        }
-    }
+
 
     return matrix;
 };
