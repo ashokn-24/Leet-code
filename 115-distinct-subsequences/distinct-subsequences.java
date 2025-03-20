f65571a714f8855702136f1121922eabba1ae530
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-03-20 23:17:36 +0530

    Time: 20 ms (44.10%) | Memory: 51.2 MB (64.87%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -8,7 +8,7 @@ class Solution {
             return 0;
         }
 
-        if( dp[i1][i2] != -1){
+        if (dp[i1][i2] != -1) {
             return dp[i1][i2];
         }
 
@@ -26,12 +26,30 @@ class Solution {
         int n = s.length();
         int m = t.length();
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n +1][m +1];
 
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
 
-        return f(n - 1, m - 1, s, t, dp);
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                char ch1 = s.charAt(i - 1);
+                char ch2 = t.charAt(j -1);
+
+                if (ch1 == ch2) {
+                    dp[i][j] = dp[i -1][j- 1] + dp[i- 1][j];
+                } else {
+                    dp[i][j] = dp[i- 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
