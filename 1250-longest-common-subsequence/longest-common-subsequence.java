6962c75c88657583372ab41e18845e639fc50d22
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-03-20 13:31:50 +0530

    Time: 21 ms (54.28%) | Memory: 50.7 MB (81.90%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -23,10 +23,27 @@ class Solution {
         int n2 = text2.length();
         int[][] dp = new int[n1 + 1][n2 + 1];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for (int i = 0; i <= n1; i++) {
+            dp[i][0] = 0;
         }
 
-        return f(n1 - 1, n2 - 1, text1, text2, dp);
+        for (int j = 0; j <= n2; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                char ch1 = text1.charAt(i - 1);
+                char ch2 = text2.charAt(j - 1);
+
+                if (ch1 == ch2) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[n1][n2];
     }
 }
