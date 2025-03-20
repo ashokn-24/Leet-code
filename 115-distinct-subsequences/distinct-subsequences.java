a380624b3d04a0a6b5aa914f97226262ae4c2667
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-03-20 23:05:24 +0530

    Time: 17 ms (84.16%) | Memory: 51.1 MB (73.86%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,37 @@
+class Solution {
+    int f(int i1, int i2, String s, String t, int[][] dp) {
+        if (i2 < 0) {
+            return 1;
+        }
+
+        if (i1 < 0) {
+            return 0;
+        }
+
+        if( dp[i1][i2] != -1){
+            return dp[i1][i2];
+        }
+
+        char ch1 = s.charAt(i1);
+        char ch2 = t.charAt(i2);
+
+        if (ch1 == ch2) {
+            return dp[i1][i2] = f(i1 - 1, i2 - 1, s, t, dp) + f(i1 - 1, i2, s, t, dp);
+        } else {
+            return dp[i1][i2] = f(i1 - 1, i2, s, t, dp);
+        }
+    }
+
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n][m];
+
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+
+        return f(n - 1, m - 1, s, t, dp);
+    }
+}
