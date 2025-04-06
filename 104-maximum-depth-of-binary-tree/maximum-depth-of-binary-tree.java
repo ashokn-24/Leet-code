6466708bfeaebe2285c4db0f5a5f6d0e00724056
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-04-06 10:03:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.92%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -1,27 +1,35 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        int lh = 1 + maxDepth(root.left);
-        int rh = 1 + maxDepth(root.right);
-
-        return Math.max(lh, rh);
+  int preorder(TreeNode node) {
+    if (node == null) {
+      return 0;
     }
+
+    int left = 1 + preorder(node.left);
+    int right = 1 + preorder(node.right);
+
+    return Math.max(left, right);
+  }
+
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    return preorder(root);
+  }
 }
