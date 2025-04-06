23cfa6ab089dbd42264cb699778751ab361a9ada
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-04-06 10:51:21 +0530

    Time: 1 ms (25.17%) | Memory: 44.5 MB (44.33%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,36 +1,48 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-
-    public int height(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        int lh = height(root.left);
-        if (lh == -1) return -1; 
-        int rh = height(root.right);
-        if (rh == -1) return -1; 
-
-        if (Math.abs(lh - rh) > 1) return -1; 
-
-        return 1 + Math.max(lh, rh); 
+  int dfs(TreeNode node) {
+    if (node == null) {
+      return 0;
     }
 
-    public boolean isBalanced(TreeNode root) {
-        return height(root) != -1;
+    int lft = 1 + dfs(node.left);
+    int rht = 1 + dfs(node.right);
+
+    return Math.max(lft, rht);
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    if (root == null) {
+      return true;
     }
+
+    int left = dfs(root.left);
+    int right = dfs(root.right);
+
+    if (Math.abs(left - right) > 1)
+      return false;
+
+    boolean lh = isBalanced(root.left);
+    boolean rh = isBalanced(root.right);
+
+    if(!lh || !rh){
+      return false;
+    }
+
+    return true;
+  }
 }
