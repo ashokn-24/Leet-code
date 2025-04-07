e29cf2bcec258924ae34009becff7df5a0086117
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-04-07 22:17:04 +0530

    Time: 43 ms (51.31%) | Memory: 47.3 MB (44.46%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,27 +1,4 @@
 class Solution {
-  public boolean f(int ind, int k, int[] nums, int[][] dp) {
-    if (k == 0) {
-      return true;
-    }
-    if (ind == 0) {
-      return nums[ind] == k;
-    }
-
-    if (dp[ind][k] != -1) {
-      return dp[ind][k] == 1;
-    }
-
-    boolean notTake = f(ind - 1, k, nums, dp);
-    boolean take = false;
-
-    if (nums[ind] <= k) {
-      take = f(ind - 1, k - nums[ind], nums, dp);
-    }
-
-    dp[ind][k] = (take || notTake) ? 1 : 0;
-    return take || notTake;
-  }
-
   public boolean canPartition(int[] nums) {
     int n = nums.length;
     int s = 0;
@@ -35,30 +12,28 @@ class Solution {
     }
 
     int k = s / 2;
+    boolean[][] dp = new boolean[n][k + 1];
 
-    boolean[] prev = new boolean[k + 1];
-    boolean[] curr = new boolean[k + 1];
-
-    prev[0] = true;
-    curr[0] = true;
+    for (int i = 0; i < n; i++) {
+      dp[i][0] = true;
+    }
 
     if (nums[0] <= k) {
-     prev[nums[0]] = true;
+      dp[0][nums[0]] = true;
     }
     for (int ind = 1; ind < n; ind++) {
       for (int tar = 1; tar <= k; tar++) {
-        boolean notTake = prev[tar];
+        boolean notTake = dp[ind - 1][tar];
         boolean take = false;
 
         if (nums[ind] <= tar) {
-          take = prev[tar - nums[ind]];
+          take = dp[ind - 1][tar - nums[ind]];
         }
 
-        curr[tar] = take || notTake;
+        dp[ind][tar] = take || notTake;
       }
-      prev = Arrays.copyOf(curr, k + 1);
     }
 
-    return prev[k];
+    return dp[n - 1][k];
   }
 }
