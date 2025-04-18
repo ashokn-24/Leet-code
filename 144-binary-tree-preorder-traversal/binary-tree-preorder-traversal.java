a215fa2c0dbf640af0f74b7c20552821b805a9b8
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-04-18 19:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (44.92%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -1,36 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-
-    public void preorder(TreeNode root, List<Integer> ans) {
-        if (root == null) {
-            return;
-        }
-
-        ans.add(root.val);
-        preorder(root.left,ans);
-        preorder(root.right,ans);
+  void preorder(TreeNode node, List<Integer> res) {
+    if (node == null) {
+      return;
     }
 
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
+    res.add(node.val);
+    if (node.left != null)
+      preorder(node.left, res);
+    if (node.right != null)
+      preorder(node.right, res);
+  }
 
-        preorder(root,ans);
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
 
-        return ans;
-    }
-    
+    preorder(root, res);
+
+    return res;
+  }
 }
