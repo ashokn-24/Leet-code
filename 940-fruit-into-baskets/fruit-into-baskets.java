90dd98b634d9ff79d3defb9280b3ca519b13d5d7
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-04-30 09:04:40 +0530

    Time: 51 ms (47.07%) | Memory: 55.2 MB (52.25%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -8,7 +8,7 @@ class Solution {
     for (int right = 0; right < arr.length; right++) {
       mp.put(arr[right], mp.getOrDefault(arr[right], 0) + 1);
 
-      while (mp.size() > 2) {
+      if (mp.size() > 2) {
         mp.put(arr[left], mp.getOrDefault(arr[left], 0) - 1);
 
         if (mp.get(arr[left]) == 0) {
